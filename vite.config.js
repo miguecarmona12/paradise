191bd7a5936a1ec1de30440ac40f56c3e28ceee8
miguecarmona12 <carmonamiguel1212@gmail.com>
2024-08-14 20:17:10 -0500

    cambio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://miguecarmona12.github.io/paradise",
+   base: "https://miguecarmona12.github.io/paradise",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
