4c646678eca2ddc561456194da9a311d69b98c84
miguecarmona12 <carmonamiguel1212@gmail.com>
2024-08-14 19:24:37 -0500

    primer commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://miguecarmona12.github.io/paradise/",
+  base: "https://miguecarmona12.github.io/paradise/",
 });
